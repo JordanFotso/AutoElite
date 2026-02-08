a80a6ed3067866d8022c8d9e8209457002c8c232
JordanFotso <achillefotso43@gmail.com>
2026-02-08 08:39:15 +0100

    fix:10

---
+++ VehiClub-API/src/main/java/com/vehiclub/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.vehiclub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Autorise toutes les origines
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*"); // Autorise tous les en-têtes
+            }
+        };
+    }
+}
