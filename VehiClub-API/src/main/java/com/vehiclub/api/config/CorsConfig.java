7f4c96c27d36c7168377a08402210c809e6100e5
JordanFotso <achillefotso43@gmail.com>
2026-02-08 03:35:41 +0100

    fix:6

--- VehiClub-API/src/main/java/com/vehiclub/api/config/CorsConfig.java
+++ VehiClub-API/src/main/java/com/vehiclub/api/config/CorsConfig.java
@@ -2,23 +2,25 @@ package com.vehiclub.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Appliquer à tous les endpoints
-                        .allowedOrigins("https://auto-elite-tp.vercel.app") // Autoriser le frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Autoriser les méthodes HTTP
-                        .allowedHeaders("*") // Autoriser tous les en-têtes
-                        .allowCredentials(true); // Autoriser les cookies et les en-têtes d'autorisation
-            }
-        };
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://auto-elite-tp.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
+        configuration.setExposedHeaders(List.of("x-auth-token"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
